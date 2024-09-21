3a11fdcb9e18920b6f05c0f4080433f0eaf7247e
kkw855 <kkw855@gmail.com>
2024-09-21 16:55:54 +0900

    🎉 init: hono

---
+++ app/api/[...route]/route.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { Config, Effect, pipe } from 'effect'
+
+export const runtime = 'edge'
+
+const apiPath = pipe(
+  Effect.gen(function* () {
+    return yield* Config.string('API_PATH')
+  }),
+  Effect.runSync,
+)
+
+const app = new Hono().basePath(apiPath)
+
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello World!',
+  })
+})
+
+export const GET = handle(app)
